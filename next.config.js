9bec2a5db605158b18851655c6ae3d548eaf82ef
Víctor Sánchez <vsanchez@themindhubcompany.com>
2026-01-30 12:30:31 +0100

    Fix seo errors

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.dinaprint.com',
-          },
-        ],
-        destination: 'https://dinaprint.com/:path*',
-        permanent: true, // ← Esto hace que sea 301
-      },
-    ]
-  },
-}
+const nextConfig = {};
 
 module.exports = nextConfig;
